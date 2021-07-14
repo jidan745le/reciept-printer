a314f8afa5629550ed2c069a9e3194ecb8102f4b
jizy1 <jizy1@yonghui.cn>
2021-07-14 14:02:34 +0800

    fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -29,8 +29,8 @@ const itemsTemplate = name => ` <div class="reciept-wrapper">
         <div style="word-break:break-all"><%=item[0]%></div>     
         <div class="table-outer">        
         <% item.forEach(function(element,i,arr){if(!!i){%>
-            <%if(i===1){%><%=element%><%}else{if(arr[1].length <== 13){%>&nbsp;<%=element%><%}else{%><%=element%><%}}%>
-        <%} }) %>        
+            <%if(i===1){%><%=element%><%}else{if(arr[1].length<14){%>&nbsp;<%=element%><%}else{%><%=element%><%}}}})%>
+           
        </div>       
        <div class="low-height"><br/></div>
     <% })  %>
