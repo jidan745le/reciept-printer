a2efa10e6db6e7e65da038c3e0f05be64504efd4
jizy1 <jizy1@yonghui.cn>
2021-07-30 13:24:16 +0800

    fix

--- src/button/dist.js
+++ src/button/dist.js
@@ -7,11 +7,44 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+require("core-js/modules/es.promise.js");
+
 var _antd = require("antd");
 
 var _react = _interopRequireDefault(require("react"));
 
 var _lodop = _interopRequireDefault(require("../utils/lodop"));
+
+function getLodopAsync(retryLimit = 10, retryInterval = 150) {
+  return new Promise((res, rej) => {
+    let time = 0;
+
+    const getIt = (resolve, reject) => {
+      if (time > retryLimit) {
+        reject({
+          code: 500,
+          message: "timeout"
+        });
+        return;
+      }
+
+      const lodop = (0, _lodop.default)();
+
+      if (!lodop) {
+        time++;
+        setTimeout(() => getIt(resolve, reject), retryInterval);
+      } else {
+        resolve({
+          code: 200,
+          data: lodop
+        });
+        return;
+      }
+    };
+
+    getIt(res, rej);
+  });
+}
 
 const PrinterButton = () => {
   const [selectedKeys, setSelectedKeys] = _react.default.useState([]);
@@ -21,15 +54,19 @@ const PrinterButton = () => {
   const lodop = _react.default.useRef({});
 
   _react.default.useEffect(() => {
-    lodop.current = (0, _lodop.default)();
-    const count = lodop.current.GET_PRINTER_COUNT();
-    const printerList = new Array(count).fill(null).map((_, index) => {
-      return {
-        name: lodop.GET_PRINTER_NAME(index),
-        key: lodop.GET_PRINTER_NAME(index + ":DriverName")
-      };
+    getLodopAsync().then(res => {
+      lodop.current = res.data;
+      const count = lodop.current.GET_PRINTER_COUNT();
+      const printerList = new Array(count).fill(null).map((_, index) => {
+        return {
+          name: lodop.GET_PRINTER_NAME(index),
+          key: lodop.GET_PRINTER_NAME(index + ":DriverName")
+        };
+      });
+      setList(printerList);
+    }).catch(e => {
+      alert(e.message);
     });
-    setList(printerList);
   }, []);
 
   function handleMenuClick(e) {
--- src/button/index.js
+++ src/button/index.js
@@ -2,18 +2,43 @@ import { Menu, Dropdown, message } from "antd";
 import React from 'react'
 import getLodop from "../utils/lodop";
 
+function getLodopAsync(retryLimit = 10,retryInterval = 150){
+    return new Promise((res,rej)=>{
+        let time = 0;
+        const getIt = (resolve,reject) =>{
+            if(time > retryLimit){
+                reject({code:500,message:"timeout"})
+                return;
+            }
+            const lodop = getLodop()
+            if(!lodop){
+                time++;
+                setTimeout(() =>getIt(resolve,reject),retryInterval);
+            }else{
+                resolve({code:200,data:lodop})
+                return;
+            }           
+        }
+        getIt(res,rej)
+    })
+}
+
 const PrinterButton = () => {
     const [selectedKeys, setSelectedKeys] = React.useState([]);
     const [printerList,setList] = React.useState([]);
 
     const lodop = React.useRef({});
     React.useEffect(()=>{
-        lodop.current = getLodop();
-        const count = lodop.current.GET_PRINTER_COUNT();
-        const printerList = new Array(count).fill(null).map((_,index)=>{
-            return {name:lodop.GET_PRINTER_NAME(index),key:lodop.GET_PRINTER_NAME(index+":DriverName")}
-        });
-        setList(printerList);
+        getLodopAsync().then(res=> {
+            lodop.current = res.data;
+            const count = lodop.current.GET_PRINTER_COUNT();
+            const printerList = new Array(count).fill(null).map((_,index)=>{
+                return {name:lodop.GET_PRINTER_NAME(index),key:lodop.GET_PRINTER_NAME(index+":DriverName")}
+            });
+            setList(printerList);
+        }).catch(e =>{
+            alert(e.message)
+        })     
     },[])
 
 
